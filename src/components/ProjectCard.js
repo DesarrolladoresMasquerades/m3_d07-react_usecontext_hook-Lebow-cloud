8d63a08bb467a5cb54e6d69bc928295b7e6f5cc4
Daniel Milan <danielmilanvera@gmail.com>
2022-03-08 12:12:42 +0100

    Auth.Context done

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,15 @@
+
+import {useContext} from "react"
+import {AuthContext} from "./../context/auth.context"
+import { ThemeContext } from "../context/theme.context";
+
 function ProjectCard(props) {
+
+  const { loggedInUser } = useContext(AuthContext)
+  const {theme} = useContext(ThemeContext)
+  
   return (
-    <div className="ProjectCard">
+    <div className={"ProjectCard" + theme}>
       <h3>{props.project.name}</h3>
       <p>{props.project.technologies}</p>
     </div>
